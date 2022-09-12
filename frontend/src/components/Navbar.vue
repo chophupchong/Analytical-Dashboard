638d2f94a7c982930eb5e39a424e1e10bf0d28c9
yjlim2022 <yj_lim2020@outlook.com>
2022-09-12 22:21:53 +0800

    reformatted navbar component for dashboard (not completed)

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,22 +1,20 @@
 <template>
-  <nav class="flex items-center justify-between">
-    <h1 class="text-2xl">Firebase Vuex Auth</h1>
 
+  <div id="nav">
+    <h2>Analytics</h2>
     <!-- for all users -->
-    <div class="">
-      <router-link to="/">Dashboard</router-link>
-    </div>
+    <router-link class="routerlink" to="/">Overview</router-link>
+    <router-link class="routerlink" to="/">Facebook</router-link>
+    <router-link class="routerlink" to="/youtube">Youtube</router-link>
+    <router-link class="routerlink" to="/">Instagram</router-link>
+    <router-link class="routerlink" to="/">Tiktok</router-link>
 
+    <div id="container">
+    <router-link class="routerlink" to="/login">Settings</router-link>
+    <router-link class="routerlink" to="/login">Logout</router-link>
+    </div>
     <!-- for logged in users -->
-    <div class="flex space-x-4">
-      <button>Logout</button>
-    </div>
-
-    <!-- for logged out users -->
-    <div class="flex space-x-4">
-      <router-link to="/login">Login</router-link>
-    </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -24,3 +22,65 @@ export default {
   name: 'navbar-component',
 };
 </script>
+
+<style scoped>
+
+h2{
+  color: white;
+}
+#container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 5px;
+  border-top:solid;
+  border-color: #31313d;
+}
+#nav {
+  display: flex;
+  flex-direction: column;
+  background-color: #1B1C1E;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100vh;
+  width: 12%;
+  top: 0;
+  overflow: auto;
+  position: fixed;
+  font-family: "Poppins", sans-serif;
+  font-weight: 300;
+  color: white;
+  font-size: 16px;
+}
+  
+
+.routerlink {   
+  display: block;
+  text-decoration: none;
+  padding: 10px;
+  margin: 2px;
+  font-weight: 600;
+  text-align:left;
+  transition-property: color, background;
+  transition-duration: 0.3s;
+  text-decoration: none;
+}
+
+  
+.routerlink:hover {
+  border-radius: 5px;
+  background: #323335;
+  min-width: 100px;
+}
+  
+.routerlink:visited {
+  border-radius: 5px;
+  min-width: 100px;
+  color: #62666b;
+}
+  
+.routerlink:link {
+  border-radius: 5px;
+  min-width: 100px;
+  color: #323335;
+}
+</style>
