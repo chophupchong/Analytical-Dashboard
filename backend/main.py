93baa5b2853ed55cf9c26d1001317a5ee1d9088d
Elton <57287843+eltontay@users.noreply.github.com>
2022-09-12 18:58:44 +0800

    randomly initiating some mock data upon linking to realtime database

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import json
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 
@@ -9,5 +10,11 @@ cred = credentials.Certificate("./serviceAccountKey.json")
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': "https://chc-dashboard-3bfea-default-rtdb.asia-southeast1.firebasedatabase.app"
 })
+
+ref = db.reference("/")
+with open("mock.json", "r") as f:
+    file_contents = json.load(f)
+
+ref.set(file_contents)
 
 app = FastAPI()
