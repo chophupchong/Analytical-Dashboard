9507101fee17e93b48defe1e738d523c5e1c0358
brandonyhr98 <brandonyhr@gmail.com>
2022-09-16 00:49:21 +0800

    testing vue charts

--- frontend/src/views/dashboard/facebook.vue
+++ frontend/src/views/dashboard/facebook.vue
@@ -1,9 +1,24 @@
 <template>
-  
+  <AreaChart/>
 </template>
 
 <script>
+import AreaChart from "../charts/AreaChart.vue";
+
 export default {
+    name: "App",
+    components: {
+        AreaChart,
+    },
+    data() {
+    return {
+      chartData: {
+        Books: 24,
+        Magazine: 30,
+        Newspapers: 10
+      }
+    };
+  }
 
 }
 </script>
