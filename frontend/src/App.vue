1d829568772b5ea3652c66bc3a162b1d6a92877d
Roopa Niedu <bitznobel@gmail.com>
2022-09-17 23:01:05 +0800

    Changed Navbar UI using Element Plus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="w-4/5 mx-auto mt-4">
-    <Navbar />
-    <router-view />
-  </div>
+  <el-container>
+    <el-aside width="200px"> <navbar-2 /></el-aside>
+    <el-main><router-view /></el-main>
+  </el-container>
 </template>
 <script>
-import Navbar from './components/Navbar.vue';
+// import Navbar from "./components/Navbar.vue";
+import Navbar2 from "./components/Navbar2.vue";
+// import Navbar2 from "./components/Navbar2.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar2 },
 };
 </script>
 <style>
@@ -19,10 +21,8 @@ export default {
   color: #2c3e50;
 }
 
-
 body {
-margin:0;
-
+  margin: 0;
 }
 nav a {
   font-weight: bold;
@@ -31,5 +31,29 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 15px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.el-aside {
+  min-height: 100vh;
 }
 </style>
