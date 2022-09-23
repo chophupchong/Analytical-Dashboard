7d7ad260a69cad44bee64c03583959b9fb055812
clementyee303 <yeeclement303@gmail.com>
2022-09-24 04:19:51 +0800

    added daily and aggregated ad metrics write and read rest apis
    added daily and aggregated facebook basic ad metrics write and read rest apis (put and get requests)

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from firebase import database
-from routers import youtube  # , facebook, instagram
+from routers import facebook#youtube  # , facebook, instagram
 
 app = FastAPI()
 app.add_middleware(
@@ -11,6 +11,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(youtube.router)
-# app.include_router(facebook.router)
+#app.include_router(youtube.router)
+app.include_router(facebook.router)
 # app.include_router(instagram.router)
