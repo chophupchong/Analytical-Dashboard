9320efecf14eed283468adbc97ac856f2fd255fd
clementyee303 <yeeclement303@gmail.com>
2022-10-12 01:31:40 +0800

    change logic for daily put request (fb)
    added new daily put request to only fetch up till the latest updated date. also added day validations. day < 0 will be changed to 1 while day > 1110 will be set to 1110 as that is the marketing API limit.

--- backend/routers/facebook.py
+++ backend/routers/facebook.py
@@ -21,7 +21,7 @@ app_id = data["app_id"]
 
 #helper method
 def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
+    for n in range(int((end_date - start_date).days)+1):
         yield start_date + datetime.timedelta(n)
 
 #test
@@ -57,8 +57,8 @@ def daterange(start_date, end_date):
 #     return json.dumps(dataset)
 
 #ETL
-@router.put("/facebook/basic-ad-metrics/aggregated/{days}")
-async def getAggregatedBasicAdMetricsByDays(days: int = 30):
+@router.put("/facebook/store/basic-ad-metrics/aggregated/{days}")
+async def updateAggregatedBasicAdMetricsByDays(days: int = 30):
     if days < 0:
         days = 30
     FacebookAdsApi.init(access_token=access_token)
@@ -108,15 +108,15 @@ async def getAggregatedBasicAdMetricsByDays(days: int = 30):
     }
     for ad_account_id in ad_account_ids:
         dataset[ad_account_id] = {
-            "reach": "0",
-            "impressions": "0",
-            "spend": "0",
+            "reach": 0,
+            "impressions": 0,
+            "spend": 0.00,
             "date_start": since_day_month_year,
             "date_stop": now_day_month_year,
             "previous_period": {
-                "reach": "0",
-                "impressions": "0",
-                "spend": "0",
+                "reach": 0,
+                "impressions": 0,
+                "spend": 0.00,
                 "date_start": prev_since_day_month_year,
                 "date_stop": prev_now_day_month_year,
             }
@@ -131,9 +131,12 @@ async def getAggregatedBasicAdMetricsByDays(days: int = 30):
             # print(dir(record))
             if (record['publisher_platform'] != "facebook"):
                 continue
-            for metric_name in record:
-                #print(metric_name + ": " + record[metric_name])
-                dataset[ad_account_id][metric_name] = record[metric_name]
+            dataset[ad_account_id]["reach"] += int(record["reach"])
+            dataset[ad_account_id]["impressions"] += int(record["impressions"])
+            dataset[ad_account_id]["spend"] += float(record["spend"])
+            # for metric_name in record:
+            #     #print(metric_name + ": " + record[metric_name])
+            #     dataset[ad_account_id][metric_name] = record[metric_name]
         ref.update({
             "reach": dataset[ad_account_id]["reach"],
             "impressions": dataset[ad_account_id]["impressions"],
@@ -157,10 +160,13 @@ async def getAggregatedBasicAdMetricsByDays(days: int = 30):
         )
 
         for record in result:
-            if (record['publisher_platform'] != "facebook"):
+            if (record['publisher_platform'] == "instagram"):
                 continue
-            for metric_name in record:
-                dataset[ad_account_id]["previous_period"][metric_name] = record[metric_name]
+            # for metric_name in record:
+            #     dataset[ad_account_id]["previous_period"][metric_name] = record[metric_name]
+            dataset[ad_account_id]["previous_period"]["reach"] += int(record["reach"])
+            dataset[ad_account_id]["previous_period"]["impressions"] += int(record["impressions"])
+            dataset[ad_account_id]["previous_period"]["spend"] += float(record["spend"])
         ref.update({
             "reach": dataset[ad_account_id]["previous_period"]["reach"],
             "impressions": dataset[ad_account_id]["previous_period"]["impressions"],
@@ -174,7 +180,9 @@ async def getAggregatedBasicAdMetricsByDays(days: int = 30):
 @router.get("/facebook/basic-ad-metrics/aggregated/{days}")
 async def getAggregatedBasicAdMetrics(days: int = 30):
     if days < 0:
-        days = 30
+        days = 1
+    if days > 1110:
+        days = 1110
     dataset = {}
     for ad_account_id in ad_account_ids:
         dataset[ad_account_id] = {}
@@ -186,10 +194,14 @@ async def getAggregatedBasicAdMetrics(days: int = 30):
    
    
 #ETL
-@router.put("/facebook/basic-ad-metrics/daily/{days}")
-async def getDailyBasicAdMetrics(days: int = 30):
+@router.put("/facebook/store/basic-ad-metrics/daily/{days}") 
+#max days is 1110
+#"message": "(#3018) The start date of the time range cannot be beyond 37 months from the current date",
+async def updateDailyBasicAdMetrics(days: int = 30):
     if days < 0:
-        days = 30
+        days = 1
+    if days > 1110:
+        days = 1110
     FacebookAdsApi.init(access_token=access_token)
     dataset = {}
     fields = [
@@ -226,12 +238,12 @@ async def getDailyBasicAdMetrics(days: int = 30):
         dataset[ad_account_id] = {}
         for single_date in daterange(since, now):
             curr_date = single_date.strftime("%Y-%m-%d")
-            prev_date = (single_date - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+            #prev_date = (single_date - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
             dataset[ad_account_id][curr_date] = {
-                "reach": "0",
-                "impressions": "0",
-                "spend": "0",
-                "date_start": prev_date,
+                "reach": 0,
+                "impressions": 0,
+                "spend": 0.00,
+                "date_start": curr_date,
                 "date_stop": curr_date
             }
         ref = db.reference(f"/facebook/{ad_account_id}/basic-ad-metrics/daily/")
@@ -239,17 +251,24 @@ async def getDailyBasicAdMetrics(days: int = 30):
             fields=fields,
             params=params,
         )
-        print(result)
+        #print(result)
         for record in result:
-            if (record['publisher_platform'] != "facebook"):
+            if (record['publisher_platform'] == "instagram"):
                 continue
-            dataset[ad_account_id][record["date_stop"]] = {
-                "reach": record["reach"],
-                "impressions": record["impressions"],
-                "spend": record["spend"],
-                "date_start": record["date_start"],
-                "date_stop": record["date_stop"]
-            }
+            #print(record)
+            #print(record["date_stop"])
+            dataset[ad_account_id][record["date_stop"]]["reach"] += int(record["reach"])
+            dataset[ad_account_id][record["date_stop"]]["impressions"] += int(record["impressions"])
+            dataset[ad_account_id][record["date_stop"]]["spend"] += float(record["spend"])
+            # dataset[ad_account_id][record["date_stop"]]["date_start"] = record["date_start"]
+            # dataset[ad_account_id][record["date_stop"]]["date_stop"] = record["date_stop"]
+            # dataset[ad_account_id][record["date_stop"]] = {
+            #     "reach": record["reach"],
+            #     "impressions": record["impressions"],
+            #     "spend": record["spend"],
+            #     "date_start": record["date_start"],
+            #     "date_stop": record["date_stop"]
+            # }
         for curr_date_key in dataset[ad_account_id]:
             ref.child(curr_date_key).update({
                 "reach": dataset[ad_account_id][curr_date_key]["reach"],
@@ -258,7 +277,104 @@ async def getDailyBasicAdMetrics(days: int = 30):
                 "date_start": dataset[ad_account_id][curr_date_key]["date_start"],
                 "date_stop": dataset[ad_account_id][curr_date_key]["date_stop"]
             })
+        #update last updated date
+        ref.child("last_updated_date").update({
+            "date": now_day_month_year
+        })
+    return json.dumps(dataset) 
+
+
+#ETL the main daily call since we dont want to call all the way to history everytime we want the "latest" data since the last time we called it
+@router.put("/facebook/store/basic-ad-metrics/latest-daily/") 
+#max days is 1110
+#"message": "(#3018) The start date of the time range cannot be beyond 37 months from the current date",
+async def updateDailyBasicAdMetrics():
+
+    FacebookAdsApi.init(access_token=access_token)
+    dataset = {}
+    fields = [
+        'reach',
+        'impressions',
+        'spend',
+        # 'quality_score_ecvr',
+        # 'quality_score_ectr',
+        # 'actions:page_engagement',
+        # 'actions:like',
+        #'actions', #most of the info is here
+    ]
     
+    now = datetime.datetime.now()
+    year = '{:02d}'.format(now.year)
+    month = '{:02d}'.format(now.month)
+    day = '{:02d}'.format(now.day)
+    now_day_month_year = '{}-{}-{}'.format(year, month, day)
+    
+    for ad_account_id in ad_account_ids:
+        ref = db.reference(f"/facebook/{ad_account_id}/basic-ad-metrics/daily/last_updated_datez")
+        since_day_month_year_date = ref.get()
+        if since_day_month_year_date == None:
+            since = now - datetime.timedelta(days=1110)
+            year = '{:02d}'.format(since.year)
+            month = '{:02d}'.format(since.month)
+            day = '{:02d}'.format(since.day)
+            since_day_month_year = '{}-{}-{}'.format(year, month, day)
+        else:
+            since_day_month_year = since_day_month_year_date["date"]
+            since = datetime.datetime.strptime(since_day_month_year, "%Y-%m-%d")
+        params = {
+            'time_range': {'since': since_day_month_year, 'until': now_day_month_year},
+            'filtering': [],
+            'level': 'account',
+            #'breakdowns': ['ad_name'],
+            'breakdowns': ['publisher_platform'],
+            "time_increment": 1, #to get daily increment https://developers.facebook.com/docs/marketing-api/insights/parameters/v15.0
+        }
+        dataset[ad_account_id] = {}
+        for single_date in daterange(since, now):
+            curr_date = single_date.strftime("%Y-%m-%d")
+            #prev_date = (single_date - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+            dataset[ad_account_id][curr_date] = {
+                "reach": 0,
+                "impressions": 0,
+                "spend": 0.00,
+                "date_start": curr_date,
+                "date_stop": curr_date
+            }
+        ref = db.reference(f"/facebook/{ad_account_id}/basic-ad-metrics/daily/")
+        result = AdAccount(ad_account_id).get_insights(
+            fields=fields,
+            params=params,
+        )
+        #print(result)
+        for record in result:
+            if (record['publisher_platform'] == "instagram"):
+                continue
+            #print(record)
+            #print(record["date_stop"])
+            dataset[ad_account_id][record["date_stop"]]["reach"] += int(record["reach"])
+            dataset[ad_account_id][record["date_stop"]]["impressions"] += int(record["impressions"])
+            dataset[ad_account_id][record["date_stop"]]["spend"] += float(record["spend"])
+            # dataset[ad_account_id][record["date_stop"]]["date_start"] = record["date_start"]
+            # dataset[ad_account_id][record["date_stop"]]["date_stop"] = record["date_stop"]
+            # dataset[ad_account_id][record["date_stop"]] = {
+            #     "reach": record["reach"],
+            #     "impressions": record["impressions"],
+            #     "spend": record["spend"],
+            #     "date_start": record["date_start"],
+            #     "date_stop": record["date_stop"]
+            # }
+        for curr_date_key in dataset[ad_account_id]:
+            ref.child(curr_date_key).update({
+                "reach": dataset[ad_account_id][curr_date_key]["reach"],
+                "impressions": dataset[ad_account_id][curr_date_key]["impressions"],
+                "spend": dataset[ad_account_id][curr_date_key]["spend"],
+                "date_start": dataset[ad_account_id][curr_date_key]["date_start"],
+                "date_stop": dataset[ad_account_id][curr_date_key]["date_stop"]
+            })
+        #update last updated date
+        ref.child("last_updated_date").update({
+            "date": now_day_month_year
+        })
     return json.dumps(dataset) 
 
 #reading from db
@@ -266,7 +382,9 @@ async def getDailyBasicAdMetrics(days: int = 30):
 async def getDailyBasicAdMetrics(days: int = 30):
     try:
         if days < 0:
-            days = 30
+            days = 1
+        if days > 1110:
+            days = 1110
         now = datetime.datetime.now()
         since = now - datetime.timedelta(days=days)
         dataset = {}
